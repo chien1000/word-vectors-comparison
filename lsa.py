a7e6bddd3ee3303f7825d084d654f68d26d3725a
chienhui <r05725004@ntu.edu.tw>
2018-04-01 19:57:41 +0800

    apply svd on tdm rather than dtm

--- lsa.py
+++ lsa.py
@@ -6,7 +6,6 @@ from sklearn.preprocessing import Normalizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 
-from stop_words import ENGLISH_STOP_WORDS # stopword list defined by sklearn
 from base import BaseWordVectorizer
 from exceptions import *
 
@@ -31,25 +30,20 @@ class LsaWordVectorizer(BaseWordVectorizer):
         dtm = self.vectorizer.fit_transform(raw_documents)
         self.vocabulary_ = self.vectorizer.vocabulary_
 
-        dtm = dtm.asfptype()
+        tdm = dtm.T
+        tdm = tdm.asfptype()
         svd = TruncatedSVD(self.vector_dim, algorithm = 'arpack')
-        svd = svd.fit(dtm)
-        self.svd = svd #components_ : n_components* n_features
-
-        # dtm_svd = svd.fit_transform(dtm) # doc_len * vector_dim
-        # dtm_svd = Normalizer(copy=False).fit_transform(dtm_svd) 
+        tdm_svd = svd.fit_transform(tdm) # vocab_len * vector_dim
+        # tdm_svd = Normalizer(copy=False).fit_transform(tdm_svd) 
+        
+        self.svd = svd #components_ : vector_dim* doc_len
+        self.tdm_svd = tdm_svd
 
     def get_similarity(self, term1, term2):
 
-        ind1 = self.vocabulary_.get(term1)
-        ind2 = self.vocabulary_.get(term2)
-        if not ind1:
-            raise KeyError('term {} is not in the vocabulary'.format(term1))
-        if not ind2:
-            raise KeyError('term {} is not in the vocabulary'.format(term2))
+        v1 = self.__getitem__(term1).reshape(1, -1)
+        v2 = self.__getitem__(term2).reshape(1, -1)
 
-        v1 = self.svd.components_[:, ind1].reshape(1, -1)
-        v2 = self.svd.components_[:, ind2].reshape(1, -1)
         sim = cosine_similarity(v1, v2)[0][0]
 
         return sim
@@ -64,7 +58,7 @@ class LsaWordVectorizer(BaseWordVectorizer):
         if not ind:
             raise KeyError('term {} is not in the vocabulary'.format(key))
 
-        word_vec =  self.svd.components_[:, ind]
+        word_vec =  self.tdm_svd[ind, :]
         return word_vec
 
 #normalization???
