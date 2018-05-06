07904e65b50ef941bde4f0e26527cde1262f6b1c
chienhui <r05725004@ntu.edu.tw>
2018-05-06 18:22:33 +0800

    MyTextCorpur inheritting gensim.corpora.textcorpus.TextCorpus, for preprocessing corpus

--- corpus.py
+++ corpus.py
@@ -1,5 +1,7 @@
 import re
 import html
+import os
+import json
 
 class Corpus(object):
     """docstring for Corpus"""
@@ -66,3 +68,106 @@ class Reuters(Corpus):
         return articles
         
         #iterator
+
+from gensim.corpora.textcorpus import TextCorpus
+from gensim.corpora.textcorpus import lower_to_unicode, strip_multiple_whitespaces, remove_stopwords, remove_short
+from gensim.utils import deaccent, simple_tokenize
+
+def remove_numbers(tokens):
+    return [token for token in tokens if not str.isdigit(token)]
+        
+class MyTextCorpus(TextCorpus):
+    #add remove_numbers
+
+    def __init__(self, input=None, dictionary=None, metadata=False, character_filters=None,
+                 tokenizer=None, token_filters=None):
+
+        # super(MyTextCorpus, self).__init__(input=input, dictionary=dictionary, metadata=metadata,
+        #     character_filters=character_filters, tokenizer=tokenizer, token_filters=token_filters)
+        # self.token_filters.append(remove_numbers)
+
+        self.input = input
+        self.metadata = metadata
+
+        self.character_filters = character_filters
+        if self.character_filters is None:
+            self.character_filters = [lower_to_unicode, deaccent, strip_multiple_whitespaces]
+
+        self.tokenizer = tokenizer
+        if self.tokenizer is None:
+            self.tokenizer = simple_tokenize
+
+        self.token_filters = token_filters
+        if self.token_filters is None:
+            self.token_filters = [remove_short, remove_stopwords, remove_numbers]
+        
+        self.length = None
+        self.dictionary = None
+        self.init_dictionary(dictionary)
+
+
+    def get_text_str(self):
+        lines = self.getstream()
+        if self.metadata:
+            for lineno, line in enumerate(lines):
+                yield ' '.join(self.preprocess_text(line)),  (lineno,)
+        else:
+            for line in lines:
+                yield ' '.join(self.preprocess_text(line))
+
+    def get_texts(self, max_sentence_length=None):
+        """Generate documents from corpus.
+        Yields
+        ------
+        list of str
+            Document as sequence of tokens (+ lineno if self.metadata)
+        """
+        lines = self.getstream()
+        
+        for lineno, line in enumerate(lines):
+            tokens = self.preprocess_text(line)
+
+            if max_sentence_length is None:
+                max_sentence_length = len(tokens) 
+            i = 0
+            while i < len(tokens):
+                if self.metadata:
+                    yield tokens[i: i + max_sentence_length], (lineno,)
+                else:
+                    yield tokens[i: i + max_sentence_length]
+
+                i += max_sentence_length
+
+    # def __iter__(self):
+    #     for text in self.get_texts():
+    #         yield text
+
+    def __getstate__(self):
+        attr = {}
+        attr['input'] = self.input
+        attr['character_filters'] = [func.__name__ for func in self.character_filters]
+        attr['token_filters'] = [func.__name__ for func in self.token_filters]
+        attr['tokenizer'] = self.tokenizer.__name__
+        attr['length'] = self.length
+
+        return attr
+
+    def write_file(self, name):
+        output_path = os.path.join('data/preprocessed', name+'.txt')
+        setting_path = os.path.join('data/preprocessed', name+'_setting.txt')
+
+        output = open(output_path, 'w')
+        for tokens in self.get_texts():
+            output.write(' '.join(tokens))
+            output.write('\n')
+        output.close()
+
+        with open(setting_path, 'w') as out:
+            json.dump(self.__getstate__(), out)
+
+
+if __name__ == '__main__':
+
+    docs = MyTextCorpus(input = 'data/wikipedia/enwiki-20180101-p30304p88444-processed.txt')
+    # docs = MyTextCorpus(input = 'data/preprocessed/reuters_docperline.txt')
+    docs.write_file('test')
