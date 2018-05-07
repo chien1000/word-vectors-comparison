b7e6dc5d34ba37f1dad57e37b1b949fd4c168198
chienhui <r05725004@ntu.edu.tw>
2018-05-08 02:29:25 +0800

    save, load

--- base.py
+++ base.py
@@ -1,7 +1,50 @@
+#! python3
 
+import os 
+import pickle
+
+import traceback
+
+MODEL_PATH = 'models'
 
 class BaseWordVectorizer(object):
-    """docstring for BaseWordVectorizer"""
+    """BaseWordVectorizer"""
     def __init__(self):
         super(BaseWordVectorizer, self).__init__()
+
+    def save_model(self, model_path=None):
+        if model_path is None:
+            model_path = MODEL_PATH
+        mid =  self.get_mid()
+        mfile = mid + '.bin'
+        mpath = os.path.join(model_path, mfile)
+
+        with open(mpath, 'wb') as fout:
+            dump_dict = {'word_vectors':self.word_vectors, 
+            'vocabulary':self.vocabulary, 'ind2word':self.ind2word}
+            pickle.dump(dump_dict, fout)
+
+    def load_model(self, model_path=None):
+        if model_path is None:
+            model_path = MODEL_PATH
+        mid =  self.get_mid()
+        mfile = mid + '.bin'
+        mpath = os.path.join(model_path, mfile)
+
+        try:
+            with open(mpath, 'rb') as fin:
+                dump_dict = pickle.load(fin)
+                self.word_vectors = dump_dict['word_vectors']
+                self.vocabulary = dump_dict['vocabulary']
+                self.ind2word = dump_dict['ind2word']
+                if hasattr(self, 'init_sims'):
+                    self.init_sims()
+
+        except FileNotFoundError as e:
+            print('model loading fails: file does not exist')
+            self.word_vectors = None
+
+        except Exception as e:
+            s = traceback.format_exc()
+            print(s)
 
