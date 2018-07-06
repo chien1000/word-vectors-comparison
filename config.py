e5d3e06892b3f70c7d7c3ec9b8a8a7ade1bf70c3
chienhui <r05725004@ntu.edu.tw>
2018-07-06 18:38:05 +0800

    add eval, min_count config

--- config.py
+++ config.py
@@ -10,18 +10,24 @@ from glove import GloveWordVectorizer
 
 run_config = {}
 # run_config['corpus_path'] = 'data/wikipedia/enwiki-20180101-p30304p88444-processed.txt'
-# run_config['corpus_path'] = 'data/preprocessed/reuters_docperline.txt'
-run_config['corpus_path'] = 'data/preprocessed/wiki_part.txt'
+run_config['corpus_path'] = 'data/preprocessed/reuters_docperline.txt'
+# run_config['corpus_path'] = 'data/preprocessed/wiki_30mt.txt'
+# run_config['corpus_path'] = 'data/preprocessed/wiki_100percent.txt'
 
 vector_dim = 100
+min_count = 0
 run_config['vector_dim'] = vector_dim
-run_config['models'] = [LsaWordVectorizer(vector_dim=vector_dim, count_normalization='tfidf'),
-                                HalWordVectorizer(max_features=vector_dim ),
-                                # CoalsWordVectorizer(window_size=4,  max_features=14000, svd_dim=vector_dim),
-                                LdaWordVectorizer(num_topics=vector_dim, alpha=50/vector_dim, eta=0.01, passes=10),
-                                W2vWordVectorizer(vector_dim, algorithm='cbow', min_count=1), 
-                                W2vWordVectorizer(vector_dim, algorithm='skip-gram', min_count=1), 
-                                GloveWordVectorizer(vector_dim, min_count=1)
+run_config['min_count'] = min_count
+run_config['models'] = [
+		LsaWordVectorizer(vector_dim=vector_dim, min_count=min_count, count_normalization='entropy'),
+		LsaWordVectorizer(vector_dim=vector_dim, min_count=min_count, count_normalization='tfidf'),
+                                HalWordVectorizer(max_features=vector_dim, min_count=min_count, window_size=8 ),
+                                CoalsWordVectorizer(window_size=4,  max_features=14000, svd_dim=vector_dim, min_count=min_count),
+                                LdaWordVectorizer(num_topics=vector_dim, alpha=50/vector_dim, eta=0.01, passes=10, min_count=min_count),
+                                W2vWordVectorizer(vector_dim, algorithm='cbow', min_count=min_count, window_size=5), 
+                                W2vWordVectorizer(vector_dim, algorithm='skip-gram', min_count=min_count, window_size=5), 
+                                GloveWordVectorizer(vector_dim, min_count=min_count, max_iter=50, window_size=5)
                                ]
-run_config['output_dir'] = 'exp_results'
-run_config['spec'] = 'wiki_part'
+run_config['output_dir'] = 'exp_results' 
+run_config['spec'] = 'test2' #'test2' #'wiki_30mt_300_try2'   'wiki_10percent_mc5_300'
+run_config['eval'] = ['anal'] #['sim', 'anal', 'ner']
